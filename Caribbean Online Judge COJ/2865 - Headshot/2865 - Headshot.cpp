e1599dc3492748ba7834e14042d6c4c60571f3b1
Angel Santiago Jaime Zavala <AnhellO@users.noreply.github.com>
2014-05-23 01:58:10 -0500

    Update 2865 - Headshot.cpp

--- Caribbean Online Judge COJ/2865 - Headshot/2865 - Headshot.cpp
+++ Caribbean Online Judge COJ/2865 - Headshot/2865 - Headshot.cpp
@@ -12,14 +12,14 @@ int main()
 	temp = shoot[N - 1];
 	for(i = 0 ; i < N ; i++)
 	{
-        if(shoot[i] == '1')
-        {
-            prob1++;
-            if(temp == '1') prob2++;
+            if(shoot[i] == '1')
+            {
+                prob1++;
+                if(temp == '1') prob2++;
+            }
+            temp = shoot[i];
         }
-        temp = shoot[i];
-    }
-    median = prob2 * N - prob1 * prob1;
-    cout << ((!median) ? "EQUAL\n" : ((median < 0) ? "ROTATE\n" : "SHOOT\n"));
+        median = prob2 * N - prob1 * prob1;
+        cout << ((!median) ? "EQUAL\n" : ((median < 0) ? "ROTATE\n" : "SHOOT\n"));
 	return 0;
 }
