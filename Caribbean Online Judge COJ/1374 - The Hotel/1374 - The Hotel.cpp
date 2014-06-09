bc2136af539e926a4f6f77ff1dcd3d594e4e046c
Angel Santiago Jaime Zavala <AnhellO@users.noreply.github.com>
2014-06-09 01:00:23 -0500

    Update 1374 - The Hotel.cpp

--- Caribbean Online Judge COJ/1374 - The Hotel/1374 - The Hotel.cpp
+++ Caribbean Online Judge COJ/1374 - The Hotel/1374 - The Hotel.cpp
@@ -6,26 +6,26 @@ int N, i, j, matrix[MAX][MAX];
 
 void fill(int N)
 {
-    int i, j, k, M, t;
-    M = N * (N + 1) / 2;
-    t = 1;
-    j = 0;
-
-    k = 1;
-    while(k <= M) 
-    {
-        for(j = N - 1 ; j >= 0 ; j--) 
-        {
-            t = j;
-            i = 0;
-            while(t <= N - 1) matrix[i++][t++] = k++;
-        }
-    }
-    for(i = 1 ; i < N ; i++) 
-    {
-        t = i;
-        for(j = 0 ; j < N - i ; ) matrix[t++][j++] = k++;
-    }
+    	int i, j, k, M, t;
+    	M = N * (N + 1) / 2;
+    	t = 1;
+    	j = 0;
+    	k = 1;
+    	
+    	while(k <= M) 
+    	{
+        	for(j = N - 1 ; j >= 0 ; j--) 
+        	{
+            		t = j;
+            		i = 0;
+            		while(t <= N - 1) matrix[i++][t++] = k++;
+        	}
+    	}
+    	for(i = 1 ; i < N ; i++) 
+    	{
+        	t = i;
+        	for(j = 0 ; j < N - i ; ) matrix[t++][j++] = k++;
+    	}
 }
 
 int main() 
