dc414a04a09c6dbc8c151d9dd7e15fbe40bc8650
Angel Santiago Jaime Zavala <AnhellO@users.noreply.github.com>
2014-07-12 14:44:26 -0500

    Update 1132 - Divisor Summation.cpp

--- Caribbean Online Judge COJ/1132 - Divisor Summation/1132 - Divisor Summation.cpp
+++ Caribbean Online Judge COJ/1132 - Divisor Summation/1132 - Divisor Summation.cpp
@@ -7,31 +7,28 @@ int sumOfDivisors(int);
 
 int main()
 {
-    scanf("%d",&n);
-    while(n--)
-    {
-        scanf("%d",&num);
-        printf("%d\n",sumOfDivisors(num) - num);
-    }
-    return 0;
+        scanf("%d",&n);
+        while(n--)
+        {
+            scanf("%d",&num);
+            printf("%d\n",sumOfDivisors(num) - num);
+        }
+        return 0;
 }
 
 int sumOfDivisors(int num)
 {
-    int p, i, prod = 1;
-    for(i = 2 ; i * i <= num ; i++)
-    {
-        p = 1;
-        while(num % i == 0)
+        int p, i, prod = 1;
+        for(i = 2 ; i * i <= num ; i++)
         {
-            p = (p * i) + 1;
-            num = num / i;
+                p = 1;
+                while(num % i == 0)
+                {
+                        p = (p * i) + 1;
+                        num /= i;
+                }
+                prod *= p;
         }
-        prod *= p;
-    }
-    if(num > 1)
-    {
-        prod *= 1 + num;
-    }
-    return prod;
+        if(num > 1) prod *= 1 + num;
+        return prod;
 }
