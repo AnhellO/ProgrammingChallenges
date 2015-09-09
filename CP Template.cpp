09adf51a649e4036f51410136c29f4480927d76c
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2015-09-09 14:40:13 -0500

    Added template for programming contests

---
+++ CP Template.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <cctype>
+#include <bitset>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <iomanip>
+#include <utility>
+#include <fstream>
+#define INF 1000000000000
+#define MAX 1000000
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<vi> vivi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vii> vvii;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+typedef map<string, int> msi;
+typedef map<int, ii> miii;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    /*ifstream file("a.in");
+	if(file) freopen("a.in", "rt", stdin);
+	ofstream output("a.out");
+    output.close();*/
+
+    return 0;
+}
+
