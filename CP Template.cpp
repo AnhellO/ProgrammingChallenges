54f0718d3f2f5fca8bb11cf71778628802f57ed6
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2015-09-14 00:35:00 -0500

    Modified CP Template

--- CP Template.cpp
+++ CP Template.cpp
@@ -29,7 +29,7 @@ using namespace std;
 typedef pair<int, int> ii;
 typedef pair<int, ii> iii;
 typedef vector<int> vi;
-typedef vector<vi> vivi;
+typedef vector<vi> vvi;
 typedef vector<ii> vii;
 typedef vector<iii> viii;
 typedef vector<vii> vvii;
@@ -39,13 +39,11 @@ typedef map<string, int> msi;
 typedef map<int, ii> miii;
 
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-    /*ifstream file("a.in");if(file)
-    freopen("a.in", "rt", stdin);
-    ofstream output("a.out");
-    output.close();*/
-
-    return 0;
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	/*ifstream file("a.in");if(file)
+	freopen("a.in", "rt", stdin);
+	ofstream output("a.out");
+	output.close();*/
+	return 0;
 }
-
