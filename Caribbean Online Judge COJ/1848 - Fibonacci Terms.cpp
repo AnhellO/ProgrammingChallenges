0f45b7e361d9e5d943f286c4add43b2ee0cb2b49
Angel Santiago Jaime Zavala <AnhellO@users.noreply.github.com>
2015-10-10 20:08:07 -0500

    Update 1848 - Fibonacci Terms.cpp

--- Caribbean Online Judge COJ/1848 - Fibonacci Terms.cpp
+++ Caribbean Online Judge COJ/1848 - Fibonacci Terms.cpp
@@ -8,21 +8,21 @@ int t, n, i, fibonacci[10001];
 int main()
 {
 	fibonacci[0] = 1 % 10000;
-    fibonacci[1] = 1 % 10000;
-    fibonacci[2] = 2 % 10000;
-    CICLO(i, 3, 10000, 1)
-        fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2] ) % 10000;
+    	fibonacci[1] = 1 % 10000;
+    	fibonacci[2] = 2 % 10000;
+    	CICLO(i, 3, 10000, 1)
+        	fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2] ) % 10000;
         
 	scanf("%d", &t);
 	LECTURA(t)
 	{
 		scanf("%d", &n);
 		if(n == 1)
-            printf("%d %d %d\n", 0, 0, 1);
-        else if(n == 2)
-            printf("%d %d %d\n", 0, 1, 1);
-        else
-        	printf("%d %d %d\n", fibonacci[n - 3], fibonacci[n - 2], fibonacci[n - 1]);
+            		printf("%d %d %d\n", 0, 0, 1);
+        	else if(n == 2)
+            		printf("%d %d %d\n", 0, 1, 1);
+        	else
+        		printf("%d %d %d\n", fibonacci[n - 3], fibonacci[n - 2], fibonacci[n - 1]);
 	}
     return 0;
 }
